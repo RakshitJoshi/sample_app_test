f19aeae5517476af122ad5ab761cda2fa4908a04
Rakshit Joshi <rakshit.joshi@softwebsolutions.com>
2013-02-22 08:19:46 +0530

    Finish user edit, update, index and destroy actions

--- spec/factories.rb
+++ spec/factories.rb
@@ -1,8 +1,12 @@
 FactoryGirl.define do
 	factory :user do
-		name "Rakshit Joshi"
-		email "rakshit.joshi@softwebsolutions.com"
+		sequence(:name) { |n| "Person-#{n}" }
+		sequence(:email) { |n| "person_#{n}@example.com" }
 		password "test"
 		password_confirmation "test"
+
+		factory :admin do
+			admin true
+		end
 	end
 end
--- spec/features/authentication_pages_spec.rb
+++ spec/features/authentication_pages_spec.rb
@@ -34,12 +34,31 @@ describe "AuthenticationPages" do
 
       it { should have_selector('title', text: user.name) }
       it { should have_link("Profile", href: user_path(user)) }
+      it { should have_link("Settings", href: edit_user_path(user)) }
       it { should have_link('Sign out', href: signout_path) }
       it { should_not have_link('Sign in', href: signin_path) }
 
       describe "followed with signout" do
         before { click_link "Sign out" }
         it { should have_link('Sign in') }
+      end
+    end
+  end
+
+  describe "authorization" do
+    describe "for non-signed in users" do
+      let(:user) { FactoryGirl.create(:user) }
+
+      describe "in the user controller" do
+        describe "visiting the edit page" do
+          before { visit edit_user_path(user) }
+          it { should have_selector('title', text: 'Sign in') }
+        end
+
+        describe "submiting the update action" do
+          before { put user_path(user) }
+          soecify { response.should redirect_to(signin_path) }
+        end
       end
     end
   end
